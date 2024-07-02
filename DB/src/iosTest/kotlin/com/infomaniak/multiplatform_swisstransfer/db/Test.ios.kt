f2d7f05e94bc717cef636c309415c091aa7a9ac4
Abdourahamane Boinaidi <abdourahamane.boinaidi@infomaniak.com>
2024-07-02 10:13:03 +0200

    core: Init unit tests

--- DB/src/iosTest/kotlin/com/infomaniak/multiplatform_swisstransfer/db/Test.ios.kt
+++ DB/src/iosTest/kotlin/com/infomaniak/multiplatform_swisstransfer/db/Test.ios.kt
@@ -19,12 +19,12 @@
 package com.infomaniak.multiplatform_swisstransfer.db
 
 import kotlin.test.Test
-import kotlin.test.assertTrue
+import kotlin.test.assertEquals
 
 class IosGreetingTest {
 
     @Test
     fun testExample() {
-        assertTrue(Greeting().greet().contains("iOS"), "Check iOS is mentioned")
+        assertEquals(4, 2 + 2, "Check for correct addition")
     }
 }
