bd3bbc4f45ac9f522d1830084ec9cf1cdde0ae8d
Abdourahamane Boinaidi <abdourahamane.boinaidi@infomaniak.com>
2024-07-02 16:59:04 +0200

    core: Add TransferManager

---
+++ Core/src/commonMain/kotlin/com/infomaniak/multiplatform_swisstransfer/managers/TransferManager.kt
@@ -0,0 +1,40 @@
+/*
+ * Infomaniak SwissTransfer - Multiplatform
+ * Copyright (C) 2024 Infomaniak Network SA
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.infomaniak.multiplatform_swisstransfer.managers
+
+import com.infomaniak.multiplatform_swisstransfer.db.RealmProvider
+import com.infomaniak.multiplatform_swisstransfer.network.ApiClientProvider
+
+/**
+ * TransferManager is responsible for orchestrating data transfer operations
+ * using Realm for local data management and an API client for network communications.
+ *
+ * This class integrates with both RealmProvider and ApiClientProvider to ensure
+ * smooth and efficient data transfers, providing a centralized management point
+ * for transfer-related activities.
+ *
+ * @property realmProvider The provider for managing Realm database operations.
+ * @property clientProvider The provider for creating and configuring HTTP clients for API communication.
+ */
+class TransferManager internal constructor(
+    private val realmProvider: RealmProvider,
+    private val clientProvider: ApiClientProvider,
+) {
+    //TODO: Implement here
+}
