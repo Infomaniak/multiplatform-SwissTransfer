ca876e8c234a0c5788e4666fe264a8c4821f9091
Abdourahamane Boinaidi <abdourahamane.boinaidi@infomaniak.com>
2024-07-11 14:20:10 +0200

    refactor: Use infomaniak build tools

--- Common/build.gradle.kts
+++ Common/build.gradle.kts
@@ -1,36 +1,10 @@
-import com.infomaniak.multiplatform_swisstranfer.utils.Versions
-import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-import org.jetbrains.kotlin.gradle.plugin.mpp.apple.XCFramework
-
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
+    id("infomaniak.kotlinMultiplatform")
 }
 
 kotlin {
-    androidTarget {
-        @OptIn(ExperimentalKotlinGradlePluginApi::class)
-        compilerOptions {
-            jvmTarget.set(JvmTarget.fromTarget(Versions.javaVersion.toString()))
-        }
-    }
-
-    val xcf = XCFramework()
-    listOf(
-        iosX64(),
-        iosArm64(),
-        iosSimulatorArm64(),
-        macosX64(),
-        macosArm64(),
-    ).forEach {
-        it.binaries.framework {
-            baseName = "Common"
-            xcf.add(this)
-            isStatic = true
-        }
-    }
-
     sourceSets {
         commonMain.dependencies {
             api(libs.kotlinx.coroutines.core)
@@ -39,17 +13,5 @@ kotlin {
         commonTest.dependencies {
             implementation(libs.kotlin.test)
         }
-    }
-}
-
-android {
-    namespace = "com.infomaniak.multiplatform_swisstransfer.common"
-    compileSdk = Versions.compileSdk
-    defaultConfig {
-        minSdk = Versions.minSdk
-    }
-    compileOptions {
-        sourceCompatibility = Versions.javaVersion
-        targetCompatibility = Versions.javaVersion
     }
 }
--- Core/build.gradle.kts
+++ Core/build.gradle.kts
@@ -1,45 +1,12 @@
-import co.touchlab.skie.configuration.DefaultArgumentInterop
-import com.infomaniak.multiplatform_swisstranfer.gradle.PublishPlugin
-import com.infomaniak.multiplatform_swisstranfer.utils.Versions
-import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-import org.jetbrains.kotlin.gradle.plugin.mpp.apple.XCFramework
-
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
     alias(libs.plugins.skie)
+    id("infomaniak.kotlinMultiplatform")
+    id("infomaniak.PublishPlugin")
 }
-apply<PublishPlugin>()
 
 kotlin {
-    withSourcesJar(publish = false)
-    androidTarget {
-        withSourcesJar(publish = true)
-        @OptIn(ExperimentalKotlinGradlePluginApi::class)
-        compilerOptions {
-            jvmTarget.set(JvmTarget.fromTarget(Versions.javaVersion.toString()))
-        }
-        publishLibraryVariants("release")
-    }
-
-    val xcframeworkName = "Core"
-    val xcf = XCFramework()
-    listOf(
-        iosX64(),
-        iosArm64(),
-        iosSimulatorArm64(),
-        macosX64(),
-        macosArm64(),
-    ).forEach {
-        it.binaries.framework {
-            baseName = xcframeworkName
-            binaryOption("bundleId", "com.infomaniak.multiplatform_swisstransfer.${xcframeworkName}")
-            xcf.add(this)
-            isStatic = true
-        }
-    }
-
     sourceSets {
         commonMain.dependencies {
             implementation(project(":Common"))
@@ -49,29 +16,5 @@ kotlin {
         commonTest.dependencies {
             implementation(libs.kotlin.test)
         }
-    }
-}
-
-skie {
-    features {
-        group {
-            DefaultArgumentInterop.Enabled(true)
-            DefaultArgumentInterop.MaximumDefaultArgumentCount(Versions.skieMaxArgumentCount)
-        }
-    }
-    build {
-        produceDistributableFramework()
-    }
-}
-
-android {
-    namespace = "com.infomaniak.multiplatform_swisstransfer"
-    compileSdk = Versions.compileSdk
-    defaultConfig {
-        minSdk = Versions.minSdk
-    }
-    compileOptions {
-        sourceCompatibility = Versions.javaVersion
-        targetCompatibility = Versions.javaVersion
     }
 }
