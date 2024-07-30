bc1279955a714327e40d3b9c556fe98c5676d54b
Abdourahamane Boinaidi <abdourahamane.boinaidi@infomaniak.com>
2024-07-30 14:09:53 +0200

    gradle: Add STMultiplatform to handle platforms publication

---
+++ buildTools/gradle/src/main/kotlin/com/infomaniak/gradle/plugins/STMultiplatformExtension.kt
@@ -0,0 +1,34 @@
+/*
+ * Infomaniak SwissTransfer - Multiplatform
+ * Copyright (C) 2024 Infomaniak Network SA
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.infomaniak.gradle.plugins
+
+import org.gradle.api.Action
+import org.gradle.api.Project
+import org.gradle.api.plugins.ExtensionAware
+
+open class STMultiplatformExtension {
+    var appleExportedProjects = listOf<Project>()
+
+    companion object {
+        internal const val EXTENSION_NAME = "kotlinMultiplatformConfig"
+
+        fun Project.kotlinMultiplatformConfig(configure: Action<STMultiplatformExtension>): Unit =
+            (this as ExtensionAware).extensions.configure(EXTENSION_NAME, configure)
+    }
+}
