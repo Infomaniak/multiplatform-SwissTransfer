9ca84c1814e4ec21047f552af9cde5cb859e5614
Kevin Boulongne <kevin.boulongne@infomaniak.com>
2024-09-03 15:52:06 +0200

    Remove jump lines after copyrights

--- STCommon/src/commonMain/kotlin/com/infomaniak/multiplatform_swisstransfer/common/interfaces/appSettings/AppSettings.kt
+++ STCommon/src/commonMain/kotlin/com/infomaniak/multiplatform_swisstransfer/common/interfaces/appSettings/AppSettings.kt
@@ -15,7 +15,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 package com.infomaniak.multiplatform_swisstransfer.common.interfaces.appSettings
 
 import com.infomaniak.multiplatform_swisstransfer.common.models.DownloadLimit
