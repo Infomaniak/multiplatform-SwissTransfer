1d61eee72c9a6b5321ae4dcc264916f793640faa
Abdourahamane Boinaidi <abdourahamane.boinaidi@infomaniak.com>
2024-10-10 12:44:29 +0200

    database: Get and upsert a Transfer

--- STDatabase/src/commonMain/kotlin/com/infomaniak/multiplatform_swisstransfer/database/cache/setting/TransfersController.kt
+++ STDatabase/src/commonMain/kotlin/com/infomaniak/multiplatform_swisstransfer/database/cache/setting/TransfersController.kt
@@ -17,12 +17,19 @@
  */
 package com.infomaniak.multiplatform_swisstransfer.database.cache.setting
 
+import com.infomaniak.multiplatform_swisstransfer.common.interfaces.transfers.Transfer
 import com.infomaniak.multiplatform_swisstransfer.database.RealmProvider
 import com.infomaniak.multiplatform_swisstransfer.database.models.transfers.TransferDB
+import io.realm.kotlin.UpdatePolicy
 import io.realm.kotlin.ext.query
 import io.realm.kotlin.query.RealmResults
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.emptyFlow
+import kotlinx.coroutines.flow.mapLatest
 import kotlin.coroutines.cancellation.CancellationException
 
+@OptIn(ExperimentalCoroutinesApi::class)
 class TransfersController(private val realmProvider: RealmProvider) {
 
     private val realm by lazy { realmProvider.realmTransfers }
@@ -30,6 +37,21 @@ class TransfersController(private val realmProvider: RealmProvider) {
     //region Get data
     @Throws(IllegalArgumentException::class, CancellationException::class)
     fun getTransfers(): RealmResults<TransferDB>? = realm?.query<TransferDB>()?.find()
+
+    @Throws(IllegalArgumentException::class, CancellationException::class)
+    fun getTransfersFlow(): Flow<List<TransferDB>> = getTransfers()?.asFlow()?.mapLatest { it.list } ?: emptyFlow()
+
+    fun getTransfer(linkUuid: String): TransferDB? {
+        return realm?.query<TransferDB>("${TransferDB::linkUuid.name} == '$linkUuid'")?.first()?.find()
+    }
+    //endregion
+
+    //region Upsert data
+    suspend fun upsert(transfer: Transfer<*>) {
+        realm?.write {
+            this.copyToRealm(TransferDB(transfer), UpdatePolicy.ALL)
+        }
+    }
     //endregion
 
     //region Update data
