2b52b66f3c1460f23ce3487c7314ce6f94988b6c
Abdourahamane Boinaidi <abdourahamane.boinaidi@infomaniak.com>
2024-10-17 14:49:42 +0200

    chore(Common): Add UploadSession

---
+++ STCommon/src/commonMain/kotlin/com/infomaniak/multiplatform_swisstransfer/common/interfaces/upload/UploadSession.kt
@@ -0,0 +1,30 @@
+/*
+ * Infomaniak SwissTransfer - Multiplatform
+ * Copyright (C) 2024 Infomaniak Network SA
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.infomaniak.multiplatform_swisstransfer.common.interfaces.upload
+
+interface UploadSession {
+    val uuid: String
+    val duration: String
+    val authorEmail: String
+    val password: String
+    val message: String
+    val numberOfDownload: Int
+    val language: String
+    val recipientsEmails: List<String>
+    val files: List<UploadFileSession>
+}
---
+++ STDatabase/src/commonMain/kotlin/com/infomaniak/multiplatform_swisstransfer/database/models/upload/UploadFileSessionDB.kt
@@ -0,0 +1,36 @@
+/*
+ * Infomaniak SwissTransfer - Multiplatform
+ * Copyright (C) 2024 Infomaniak Network SA
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.infomaniak.multiplatform_swisstransfer.database.models.upload
+
+import com.infomaniak.multiplatform_swisstransfer.common.interfaces.upload.UploadFileSession
+import io.realm.kotlin.types.EmbeddedRealmObject
+
+class UploadFileSessionDB() : UploadFileSession, EmbeddedRealmObject {
+    override var name: String = ""
+    override var size: Long = 0L
+    override var mimeType: String = ""
+    override var localPath: String = ""
+    var remoteUploadFile: UploadFileDB? = null
+
+    constructor(uploadFileSession: UploadFileSession) : this() {
+        this.name = uploadFileSession.name
+        this.size = uploadFileSession.size
+        this.mimeType = uploadFileSession.mimeType
+        this.localPath = uploadFileSession.localPath
+    }
+}
