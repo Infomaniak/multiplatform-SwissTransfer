9c064b95cc78917185ddc17858808c3d33366614
Kevin Boulongne <kevin.boulongne@infomaniak.com>
2024-10-17 13:03:35 +0200

    feat: Delete Realm when migration is needed

--- STDatabase/src/commonMain/kotlin/com/infomaniak/multiplatform_swisstransfer/database/RealmProvider.kt
+++ STDatabase/src/commonMain/kotlin/com/infomaniak/multiplatform_swisstransfer/database/RealmProvider.kt
@@ -59,18 +59,21 @@ class RealmProvider(private val loadDataInMemory: Boolean = false) {
     private val realmAppSettingsConfiguration = RealmConfiguration
         .Builder(schema = setOf(AppSettingsDB::class))
         .name("AppSettings")
+        .deleteRealmIfMigrationNeeded() // TODO: Remove before going to production !
         .loadDataInMemoryIfNeeded()
         .build()
 
     private val realmUploadDBConfiguration = RealmConfiguration
         .Builder(schema = setOf(UploadDB::class, UploadContainerDB::class, UploadFileDB::class))
         .name("Uploads")
+        .deleteRealmIfMigrationNeeded() // TODO: Remove before going to production !
         .loadDataInMemoryIfNeeded()
         .build()
 
     private fun realmTransfersConfiguration(userId: Int) = RealmConfiguration
         .Builder(schema = setOf(TransferDB::class, ContainerDB::class, FileDB::class))
         .name(transferRealmName(userId))
+        .deleteRealmIfMigrationNeeded() // TODO: Remove before going to production !
         .loadDataInMemoryIfNeeded()
         .build()
 
