715bd9b4f964f05eb6f49f244a82500b324123e0
Abdourahamane Boinaidi <abdourahamane.boinaidi@infomaniak.com>
2024-10-18 09:23:10 +0200

    chore: Add NewUploadSession

---
+++ STCore/src/commonMain/kotlin/com/infomaniak/multiplatform_swisstransfer/data/NewUploadSession.kt
@@ -0,0 +1,38 @@
+/*
+ * Infomaniak SwissTransfer - Multiplatform
+ * Copyright (C) 2024 Infomaniak Network SA
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.infomaniak.multiplatform_swisstransfer.data
+
+import com.infomaniak.multiplatform_swisstransfer.common.interfaces.upload.UploadContainer
+import com.infomaniak.multiplatform_swisstransfer.common.interfaces.upload.UploadFileSession
+import com.infomaniak.multiplatform_swisstransfer.common.interfaces.upload.UploadSession
+
+data class NewUploadSession(
+    override val duration: String,
+    override val authorEmail: String,
+    override val password: String,
+    override val message: String,
+    override val numberOfDownload: Int,
+    override val language: String,
+    override val recipientsEmails: List<String>,
+    override val files: List<UploadFileSession>,
+) : UploadSession {
+    override val uuid: String = ""
+    override val remoteContainer: UploadContainer? = null
+    override val remoteUploadHost: String? = null
+
+}
