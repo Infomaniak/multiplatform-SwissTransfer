b9292080f4a0c7105b2c014a86e8fad0ff60f6b5
Abdourahamane Boinaidi <abdourahamane.boinaidi@infomaniak.com>
2024-11-13 09:59:09 +0100

    feat: Update a transfer form remote

--- STCore/src/commonMain/kotlin/com/infomaniak/multiplatform_swisstransfer/managers/TransferManager.kt
+++ STCore/src/commonMain/kotlin/com/infomaniak/multiplatform_swisstransfer/managers/TransferManager.kt
@@ -26,6 +26,8 @@ import com.infomaniak.multiplatform_swisstransfer.common.models.TransferDirectio
 import com.infomaniak.multiplatform_swisstransfer.common.models.TransferStatus
 import com.infomaniak.multiplatform_swisstransfer.common.utils.mapToList
 import com.infomaniak.multiplatform_swisstransfer.database.controllers.TransferController
+import com.infomaniak.multiplatform_swisstransfer.exceptions.NotFoundException
+import com.infomaniak.multiplatform_swisstransfer.exceptions.NullPropertyException
 import com.infomaniak.multiplatform_swisstransfer.network.ApiClientProvider
 import com.infomaniak.multiplatform_swisstransfer.network.exceptions.ApiException
 import com.infomaniak.multiplatform_swisstransfer.network.exceptions.NetworkException
@@ -77,12 +79,33 @@ class TransferManager internal constructor(
     /**
      * Update all pending transfers in database, most transfers are in [TransferStatus.WAIT_VIRUS_CHECK] status.
      */
+    @Throws(RealmException::class, CancellationException::class)
     suspend fun fetchWaitingTransfers(): Unit = withContext(Dispatchers.IO) {
         val transfers = transferController.getNotReadyTransfers()
         transfers.forEach { transfer ->
             runCatching {
                 addTransferByLinkUUID(transfer.linkUUID, null)
             }
+        }
+    }
+
+    /**
+     * Update the local transfer with remote api
+     *
+     * @throws RealmException An error has occurred with realm database
+     * @throws NotFoundException Any transfer with [transferUUID] has been found
+     * @throws NullPropertyException The transferDirection of the transfer found is null
+     */
+    @Throws(RealmException::class, NotFoundException::class, NullPropertyException::class, CancellationException::class)
+    suspend fun fetchTransfer(transferUUID: String): Unit {
+        val localTransfer = transferController.getTransfer(transferUUID)
+            ?: throw NotFoundException("No transfer found in DB with uuid = $transferUUID")
+        val transferDirection = localTransfer.transferDirection
+            ?: throw NullPropertyException("the transferDirection property cannot be null")
+
+        runCatching {
+            val remoteTransfer = transferRepository.getTransferByLinkUUID(transferUUID).data ?: return
+            transferController.upsert(remoteTransfer, transferDirection)
         }
     }
 
