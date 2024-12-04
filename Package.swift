b2cfa700b98946b5ed9f631ced098ee7d61bdac6
Abdourahamane Boinaidi <abdourahamane.boinaidi@infomaniak.com>
2024-12-04 16:50:18 +0100

    chore: New release 0.9.4

--- Package.swift
+++ Package.swift
@@ -15,18 +15,18 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Core",
-            url: "https://github.com/Infomaniak/multiplatform-SwissTransfer/releases/download/0.9.3/STCore.xcframework.zip",
-            checksum: "9aec9bb083a80efc8f4c6b7561a8923c0a99b2d43a91e3abe2371100448f6d4c"
+            url: "https://github.com/Infomaniak/multiplatform-SwissTransfer/releases/download/0.9.4/STCore.xcframework.zip",
+            checksum: "c24adac9483d65b5a81f6b9d5e9c3b16faf56db5c805998b789826436739818f"
         ),
         .binaryTarget(
             name: "Database",
-            url: "https://github.com/Infomaniak/multiplatform-SwissTransfer/releases/download/0.9.3/STDatabase.xcframework.zip",
-            checksum: "12aeb4adec3a0e622ab18b5a8338ea69dbba40bb0dd057887754d2a82f25d997"
+            url: "https://github.com/Infomaniak/multiplatform-SwissTransfer/releases/download/0.9.4/STDatabase.xcframework.zip",
+            checksum: "cbd83562b4813751555437a9a6c6678d0f9645cf65a37ea83ef19096547f6395"
         ),
         .binaryTarget(
             name: "Network",
-            url: "https://github.com/Infomaniak/multiplatform-SwissTransfer/releases/download/0.9.3/STNetwork.xcframework.zip",
-            checksum: "95c9b37ce09f2531caf3670bc346d4f63f2a90d55a5525a3adbc40d964a2d81b"
+            url: "https://github.com/Infomaniak/multiplatform-SwissTransfer/releases/download/0.9.4/STNetwork.xcframework.zip",
+            checksum: "9320cdee2ae2c75d83389186c3b19525286addcc7e0b7ee6bf5168d7c14d28b5"
         ),
     ]
 )
--- buildTools/gradle/src/main/kotlin/com/infomaniak/gradle/utils/Versions.kt
+++ buildTools/gradle/src/main/kotlin/com/infomaniak/gradle/utils/Versions.kt
@@ -20,7 +20,7 @@ package com.infomaniak.gradle.utils
 import org.gradle.api.JavaVersion
 
 internal object Versions {
-    const val mavenVersionName = "0.9.3"
+    const val mavenVersionName = "0.9.4"
 
     const val minSdk = 24
     const val compileSdk = 35
