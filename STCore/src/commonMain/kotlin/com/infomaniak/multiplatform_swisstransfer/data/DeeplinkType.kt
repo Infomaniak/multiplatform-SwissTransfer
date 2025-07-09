cb6c69a12083c5ce9366d057e42601463069452f
Valentin Perignon <valentin.perignon@infomaniak.com>
2025-07-09 13:34:57 +0200

    feat: Add `DeeplinkType` and detect type from URL

---
+++ STCore/src/commonMain/kotlin/com/infomaniak/multiplatform_swisstransfer/data/DeeplinkType.kt
@@ -0,0 +1,55 @@
+/*
+ * Infomaniak SwissTransfer - Multiplatform
+ * Copyright (C) 2025 Infomaniak Network SA
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.infomaniak.multiplatform_swisstransfer.data
+
+interface DeeplinkType {
+    val uuid: String
+
+    data class DeleteTransfer(override val uuid: String, val token: String): DeeplinkType {
+        companion object {
+            private val REGEX = "^https://.+/d/(?<uuid>[^?]+)(\\?delete=(?<token>.+))?$".toRegex()
+
+            fun fromURL(url: String): DeleteTransfer? {
+                var matchResult = REGEX.matchEntire(url) ?: return null
+
+                val uuid = matchResult.groups["uuid"]?.value ?: return null
+                val token = matchResult.groups["token"]?.value ?: return null
+                return DeleteTransfer(uuid, token)
+            }
+        }
+    }
+
+    data class OpenTransfer(override val uuid: String): DeeplinkType {
+        companion object {
+            private val REGEX = "^https://.+/d/(?<uuid>[^?]+)$".toRegex()
+
+            fun fromURL(url: String): OpenTransfer? {
+                var matchResult = REGEX.matchEntire(url) ?: return null
+
+                val uuid = matchResult.groups["uuid"]?.value ?: return null
+                return OpenTransfer(uuid)
+            }
+        }
+    }
+
+    companion object {
+        fun fromURL(url: String): DeeplinkType? {
+            return DeleteTransfer.fromURL(url) ?: OpenTransfer.fromURL(url)
+        }
+    }
+}
