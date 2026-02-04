1548ef670a6c9c3b56ed50a0e1ad8d2108c27d20
Abdourahamane Boinaidi <abdourahamane.boinaidi@infomaniak.com>
2026-02-04 15:34:50 +0100

    chore: Implement room database

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,8 @@ plugins {
     alias(libs.plugins.kotlinMultiplatform).apply(false)
     alias(libs.plugins.realm).apply(false)
     alias(libs.plugins.skie) apply false
+    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.androidx.room) apply false
     alias(libs.plugins.nmcp.aggregation)
 }
 
