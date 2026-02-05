20dc7ae43c8f5b776a5f6244b2cf08f6f072b1d8
Abdourahamane Boinaidi <abdourahamane.boinaidi@infomaniak.com>
2026-02-05 17:04:10 +0100

    chore: Add room entities

---
+++ STDatabase/src/commonMain/kotlin/com/infomaniak/multiplatform_swisstransfer/database/models/transfers/v2/TransferDB.kt
@@ -0,0 +1,63 @@
+/*
+ * Infomaniak SwissTransfer - Multiplatform
+ * Copyright (C) 2026 Infomaniak Network SA
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.infomaniak.multiplatform_swisstransfer.database.models.transfers.v2
+
+import androidx.room.Embedded
+import androidx.room.Entity
+import androidx.room.Ignore
+import androidx.room.PrimaryKey
+import androidx.room.Relation
+import com.infomaniak.multiplatform_swisstransfer.common.interfaces.transfers.v2.File
+import com.infomaniak.multiplatform_swisstransfer.common.interfaces.transfers.v2.Transfer
+import com.infomaniak.multiplatform_swisstransfer.common.models.TransferDirection
+import com.infomaniak.multiplatform_swisstransfer.common.models.TransferStatus
+
+@Entity
+data class TransferDB(
+    @PrimaryKey
+    override val id: String,
+    override val senderEmail: String,
+    override val title: String?,
+    override val message: String?,
+    override val createdAt: Long,
+    override val expiresAt: Long,
+    override val totalSize: Long,
+    // Local
+    override val password: String? = null,
+    override val transferDirection: TransferDirection,
+    override val transferStatus: TransferStatus,
+    override val recipientsEmails: Set<String> = emptySet(),
+    val linkId: String? = null,
+    val userOwnerId: Long,
+) : Transfer {
+    @Ignore
+    override var files: List<File> = emptyList()
+        internal set
+}
+
+internal data class TransferWithFiles(
+    @Embedded
+    val transfer: TransferDB,
+    @Relation(
+        parentColumn = "id",
+        entityColumn = "transferId"
+    )
+    val files: List<FileDB>
+) {
+    fun toTransferDB(): TransferDB = transfer.apply { files = this@TransferWithFiles.files }
+}
