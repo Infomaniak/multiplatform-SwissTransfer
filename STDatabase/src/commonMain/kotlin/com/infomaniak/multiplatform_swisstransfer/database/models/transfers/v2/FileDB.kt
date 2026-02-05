50f20de3af7964210e4f1ac3f089ce5e88cfefef
Abdourahamane Boinaidi <abdourahamane.boinaidi@infomaniak.com>
2026-02-05 17:04:10 +0100

    chore: Add room entities

---
+++ STDatabase/src/commonMain/kotlin/com/infomaniak/multiplatform_swisstransfer/database/models/transfers/v2/FileDB.kt
@@ -0,0 +1,49 @@
+/*
+ * Infomaniak SwissTransfer - Multiplatform
+ * Copyright (C) 2026 Infomaniak Network SA
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.infomaniak.multiplatform_swisstransfer.database.models.transfers.v2
+
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.Ignore
+import androidx.room.PrimaryKey
+import com.infomaniak.multiplatform_swisstransfer.common.interfaces.transfers.v2.File
+
+@Entity(
+    foreignKeys = [ForeignKey(
+        entity = TransferDB::class,
+        parentColumns = arrayOf("id"),
+        childColumns = arrayOf("transferId"),
+        onDelete = ForeignKey.CASCADE,
+    )]
+)
+data class FileDB(
+    @PrimaryKey
+    override val id: String,
+    override val path: String,
+    override val size: Long,
+    override val mimeType: String?,
+    //Local
+    override val isFolder: Boolean,
+    val transferId: String,
+    val folderId: String? = null,
+) : File {
+    @Ignore
+    val children: List<FileDB> = emptyList()
+    @Ignore
+    val folder: FileDB? = null
+}
