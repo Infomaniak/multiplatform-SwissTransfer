be3a4abe2996a80d202888bd0516ca5e5e0116fc
Abdourahamane Boinaidi <abdourahamane.boinaidi@infomaniak.com>
2026-02-06 15:55:05 +0100

    chore: Add FileUtilsForApiV2

---
+++ STDatabase/src/commonMain/kotlin/com/infomaniak/multiplatform_swisstransfer/database/utils/FileUtilsForApiV2.kt
@@ -0,0 +1,93 @@
+/*
+ * Infomaniak SwissTransfer - Multiplatform
+ * Copyright (C) 2024 Infomaniak Network SA
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.infomaniak.multiplatform_swisstransfer.database.utils
+
+import androidx.collection.ArraySet
+import com.infomaniak.multiplatform_swisstransfer.common.interfaces.transfers.v2.File
+import com.infomaniak.multiplatform_swisstransfer.database.models.transfers.v2.FileDB
+import kotlin.uuid.ExperimentalUuidApi
+import kotlin.uuid.Uuid
+
+object FileUtilsForApiV2 {
+
+    fun getFileDbTree(transferId: String, files: List<File>): Set<FileDB> {
+        val folderByPath = HashMap<String, FileDB>(files.size)
+        val out = ArraySet<FileDB>(files.size * 2)
+
+        for (file in files) {
+            val parentId = ensureFolders(file.path, file.size, transferId, folderByPath, out)
+            out += FileDB(file, transferId, parentId)
+        }
+        return out
+    }
+
+    private fun ensureFolders(
+        filePath: String,
+        fileSize: Long,
+        transferId: String,
+        folderByPath: MutableMap<String, FileDB>,
+        out: MutableSet<FileDB>,
+    ): String? {
+        val lastSlash = filePath.lastIndexOf('/')
+        if (lastSlash <= 0) return null // No parent folder (root file)
+
+        var parentId: String? = null
+        val pathBuilder = StringBuilder(lastSlash)
+
+        var currentIndex = 0
+        while (currentIndex < lastSlash) {
+            val nextSlash = filePath.indexOf('/', currentIndex).takeIf { it != -1 } ?: lastSlash
+
+            // Skip duplicated separators
+            if (nextSlash == currentIndex) {
+                currentIndex++
+                continue
+            }
+
+            if (pathBuilder.isNotEmpty()) pathBuilder.append('/')
+            pathBuilder.append(filePath, currentIndex, nextSlash)
+
+            val folderPath = pathBuilder.toString()
+            val folder = folderByPath[folderPath] ?: generateFolderId(transferId).let { newId ->
+                FileDB(
+                    id = newId,
+                    path = folderPath,
+                    size = 0L,
+                    mimeType = null,
+                    isFolder = true,
+                    transferId = transferId,
+                    folderId = parentId
+                ).also {
+                    folderByPath[folderPath] = it
+                    out += it
+                }
+            }
+
+            folder.size += fileSize
+
+            parentId = folder.id
+            currentIndex = nextSlash + 1
+        }
+        return parentId
+    }
+
+    @OptIn(ExperimentalUuidApi::class)
+    private fun generateFolderId(transferId: String): String {
+        return "$transferId:${Uuid.random()}"
+    }
+}
