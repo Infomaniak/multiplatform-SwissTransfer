7bedcdd6eb0044057ec89fe8c61f414d8ebcdf43
Gibran Chevalley <gibran.chevalley@infomaniak.com>
2026-02-16 16:49:14 +0100

    feat: Setup maven central

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,29 @@ plugins {
     alias(libs.plugins.kotlinMultiplatform).apply(false)
     alias(libs.plugins.realm).apply(false)
     alias(libs.plugins.skie) apply false
+    alias(libs.plugins.nmcp.aggregation)
+}
+
+nmcpAggregation {
+    centralPortal {
+        username = getPropertyValue(project, "ossrhUsername")
+        password = getPropertyValue(project, "ossrhPassword")
+
+        // `publishingType` can be replaced w/ USER_MANAGED to validate on the web.
+        publishingType = "AUTOMATIC" // This is the default, but we surface it.
+    }
+}
+
+dependencies {
+    nmcpAggregation(project(":STCommon"))
+    nmcpAggregation(project(":STCore"))
+    nmcpAggregation(project(":STDatabase"))
+    nmcpAggregation(project(":STNetwork"))
+}
+
+private fun getPropertyValue(project: Project, propertyName: String): String? {
+    if (project.hasProperty(propertyName)) return project.property(propertyName) as String
+
+    val systemValue: String? = System.getenv(propertyName)
+    return systemValue
 }
