a9b818f7c1d1c85fca4c4bad18b267cc0430bd9b
Abdourahamane Boinaidi <abdourahamane.boinaidi@infomaniak.com>
2026-03-05 17:30:49 +0100

    refactor: Update toTransferUiList to improve the readability and avoid useless async

--- STCore/src/commonMain/kotlin/com/infomaniak/multiplatform_swisstransfer/mappers/TransferMapper.kt
+++ STCore/src/commonMain/kotlin/com/infomaniak/multiplatform_swisstransfer/mappers/TransferMapper.kt
@@ -23,9 +23,7 @@ import com.infomaniak.multiplatform_swisstransfer.common.interfaces.ui.TransferU
 import com.infomaniak.multiplatform_swisstransfer.database.dao.TransferDao
 import com.infomaniak.multiplatform_swisstransfer.database.models.transfers.v2.FileDB
 import com.infomaniak.multiplatform_swisstransfer.database.models.transfers.v2.TransferDB
-import kotlinx.coroutines.async
-import kotlinx.coroutines.awaitAll
-import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.mapLatest
 
@@ -66,13 +64,11 @@ internal fun FileDB.toFileUi(): FileUi = FileUi(
 )
 
 internal suspend fun List<TransferDB>.toTransferUiList(transferDao: TransferDao): List<TransferUi> {
-    val filesByTransfer = coroutineScope {
-        buildMap {
-            this@toTransferUiList.chunked(1000)
-                .map { async { transferDao.getTransferRootFiles(it.map { transfer -> transfer.id }) } }
-                .awaitAll()
-                .forEach { putAll(it) }
-        }
+    val filesByTransfer: Map<String, List<FileDB>> = buildMap {
+        this@toTransferUiList.chunked(1000).map { transfers ->
+            val transferIds = transfers.map { transfer -> transfer.id }
+            transferDao.getTransferRootFiles(transferIds)
+        }.forEach { putAll(it) }
     }
     return this.map {
         it.toTransferUi(
@@ -82,7 +78,7 @@ internal suspend fun List<TransferDB>.toTransferUiList(transferDao: TransferDao)
     }
 }
 
-@OptIn(kotlinx.coroutines.ExperimentalCoroutinesApi::class)
+@OptIn(ExperimentalCoroutinesApi::class)
 internal fun Flow<List<TransferDB>>.toTransferUiListFlow(transferDao: TransferDao): Flow<List<TransferUi>> {
     return this.mapLatest { it.toTransferUiList(transferDao) }
 }
