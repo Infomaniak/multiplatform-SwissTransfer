452e46f07f6e5d1e6af45193b17bddc0392615d4
Louis CAD <louis.cognault@infomaniak.com>
2026-07-28 16:53:20 +0200

    chore: Remove unused transfersCountFlow in TransferDao

--- STDatabase/src/commonMain/kotlin/com/infomaniak/multiplatform_swisstransfer/database/dao/TransferDao.kt
+++ STDatabase/src/commonMain/kotlin/com/infomaniak/multiplatform_swisstransfer/database/dao/TransferDao.kt
@@ -76,18 +76,6 @@ interface TransferDao {
         currentTime: Long = Clock.System.now().epochSeconds,
     ): Flow<List<TransferDB>>
 
-    @Query(
-        """SELECT count(*) FROM TransferDB 
-        WHERE userOwnerId=:userId AND transferStatus!=:uploadStatus AND transferDirection=:direction AND
-        (organizationAccountId=:organizationAccountId OR organizationAccountId IS NULL)"""
-    )
-    fun transfersCountFlow(
-        userId: Long,
-        organizationAccountId: Long?,
-        direction: TransferDirection,
-        uploadStatus: TransferStatus = TransferStatus.PENDING_UPLOAD,
-    ): Flow<Int>
-
     @Query("SELECT * FROM TransferDB WHERE userOwnerId=:userId AND id=:transferId LIMIT 1")
     fun transferFlow(userId: Long, transferId: String): Flow<TransferDB?>
 
