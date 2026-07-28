27b6be16bb56633fa6664ab28d16d704ca60cf0c
Louis CAD <louis.cognault@infomaniak.com>
2026-07-28 15:41:53 +0200

    chore: Remove unused import

--- STDatabase/src/commonMain/kotlin/com/infomaniak/multiplatform_swisstransfer/database/dao/OrganizationsDao.kt
+++ STDatabase/src/commonMain/kotlin/com/infomaniak/multiplatform_swisstransfer/database/dao/OrganizationsDao.kt
@@ -18,7 +18,6 @@
 package com.infomaniak.multiplatform_swisstransfer.database.dao
 
 import androidx.room.Dao
-import androidx.room.Delete
 import androidx.room.Query
 import androidx.room.Upsert
 import com.infomaniak.multiplatform_swisstransfer.database.models.OrganizationAccount
