4759e53b63e7ab544ef7ef2a3391f5a3ba438a80
Philippe Weidmann <philippe.weidmann@ik.me>
2026-09-11 15:42:00 +0200

    feat: Allow modules to compile on macOS

--- STNetwork/build.gradle.kts
+++ STNetwork/build.gradle.kts
@@ -30,5 +30,9 @@ kotlin {
         iosMain.dependencies {
             implementation(libs.ktor.client.darwin)
         }
+
+        macosMain.dependencies {
+            implementation(libs.ktor.client.darwin)
+        }
     }
 }
